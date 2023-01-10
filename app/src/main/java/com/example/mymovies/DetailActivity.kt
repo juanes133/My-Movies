9ed1d396dbe98c25f22e22f410181aec53e3fe8b
Juan <juanes1331@gmail.com>
2023-01-10 15:56:23 -0500

    Se actualiza icono de la aplicación y se agrega una animación

--- app/src/main/java/com/example/mymovies/DetailActivity.kt
+++ app/src/main/java/com/example/mymovies/DetailActivity.kt
@@ -1,20 +1,28 @@
 package com.example.mymovies
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.SpannableStringBuilder
+import android.view.animation.Animation
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.text.bold
 import androidx.core.text.buildSpannedString
+import com.airbnb.lottie.LottieAnimationView
 import com.bumptech.glide.Glide
 import com.example.mymovies.databinding.ActivityDetailBinding
 import com.example.mymovies.model.Movie
+import kotlinx.android.synthetic.main.activity_detail.*
 
 class DetailActivity : AppCompatActivity() {
 
     companion object {
         const val EXTRA_MOVIE = "DetailActivity:movie"
     }
+
+    var like4 = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +40,9 @@ class DetailActivity : AppCompatActivity() {
             bindDetalInfo(binding.detailInfo, movie)
         }
         setContentView(binding.root)
+        like4ImageView.setOnClickListener {
+            like4 = likeAnimation(like4ImageView, R.raw.apple_event, like4)
+        }
     }
 
     private fun bindDetalInfo(detailInfo: TextView, movie: Movie) {
@@ -44,11 +55,36 @@ class DetailActivity : AppCompatActivity() {
         }
     }
 
-    private fun SpannableStringBuilder.appendInfo(stringRes: Int, value: String){
+    private fun SpannableStringBuilder.appendInfo(stringRes: Int, value: String) {
         bold {
             append(getString(stringRes))
             append(": ")
         }
         appendLine(value)
+    }
+
+    private fun likeAnimation(
+        imageView: LottieAnimationView,
+        animation: Int,
+        like: Boolean,
+    ): Boolean {
+        if (!like) {
+            imageView.setAnimation(animation)
+            imageView.playAnimation()
+        } else {
+            imageView.animate()
+                .alpha(0f)
+                .setDuration(200)
+                .setListener(object : AnimatorListenerAdapter() {
+
+                    override fun onAnimationEnd(animator: Animator) {
+
+                        imageView.setImageResource(R.mipmap.ic_favorite)
+                        imageView.alpha = 1f
+                    }
+
+                })
+        }
+        return !like
     }
 }
